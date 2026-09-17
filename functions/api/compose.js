3188329c41ef427ae5e1836d3d76943f7acd3db1
Windwalker <38275925+821920046@users.noreply.github.com>
2026-09-17 23:33:25 +0800

    Update compose.js

--- functions/api/compose.js
+++ functions/api/compose.js
@@ -1,5 +1,5 @@
-import assets from '../../02_Assets/assets.json' with { type: 'json' };
-import profiles from '../../02_Assets/model-profiles.json' with { type: 'json' };
+import assets from '../../02_Assets/assets.json';
+import profiles from '../../02_Assets/model-profiles.json';
 import { createRegistry } from '../../src/domain/registry.js';
 import { planFilm } from '../../src/domain/planner.js';
 import { compileShot } from '../../src/domain/compiler.js';
